e6fd74c4876d85d63ed840323f6badc51929c76b
John Stovin <john.d.stovin@gmail.com>
2015-08-18 16:34:35 +0100

    Placement only in unoccupied cells

--- Blokus.Test/Blokus.Tests.fs
+++ Blokus.Test/Blokus.Tests.fs
@@ -19,3 +19,20 @@
             for cell in GameBoard.Cells do
                 cell |> should equal Empty
             
+        let checkCellColour board x y cell = 
+            board.Cells |> Array2D.iteri (fun cellX cellY value -> 
+                if cellX = x && cellY = y                
+                then value |> should equal cell 
+                else value |> should equal Empty) 
+        
+        [<Test>]
+        let ``Can place cell at 0,0 on empty board``() =
+            let newboard = placeCell GameBoard 0 0 Red
+            checkCellColour newboard 0 0 Red
+                
+        [<Test>]
+        let ``Cannot place cell at 0,0 if it is already occupied``() =
+            let board = placeCell GameBoard 0 0 Red
+            let newboard = placeCell board 0 0 Blue
+            checkCellColour newboard 0 0 Red
+
--- Blokus/Model.fs
+++ Blokus/Model.fs
@@ -2,8 +2,26 @@
 
 type Cell = 
     | Empty
+    | Blue
+    | Green 
+    | Red
+    | Yellow
 
 type Board = {X : int; Y : int; Cells : Cell [,] }
 
-let GameBoard = {X=20; Y=20; Cells = Array2D.init 20 20 (fun _ _ -> Empty) }
+let initializeBoard x y =
+    {X = x; Y = y; Cells = Array2D.init x y (fun _ _ -> Empty)}
+
+let replaceCell cells x y cell = 
+    let newCells = Array2D.copy cells
+    Array2D.set newCells x y cell
+    newCells
+
+let placeCell board x y cell =
+    match board.Cells.[x,y] with
+    | Empty -> {board with Cells = replaceCell board.Cells x y cell }
+    | _ -> board
+
+
+let GameBoard = initializeBoard 20 20
 
